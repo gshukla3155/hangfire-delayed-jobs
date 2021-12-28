34a6ad37806642d7e1682f5c9be6230a1df2e128
Ghanshyam Shukla <70849493+gshukla3155@users.noreply.github.com>
2021-12-28 20:39:27 +0530

    Project Added

---
+++ Hangfire.Service/Services/Feedback.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangfire.Service.Services
+{
+    public class Feedback
+    {
+        [Queue("feedback")]
+        public void Send(string delay)
+        {
+            Console.WriteLine("**********************************************");
+            Console.WriteLine($"Feedback.Send method called with delay of {delay}");
+            Console.WriteLine("Feedback sent");
+            Console.WriteLine("**********************************************");
+        }
+    }
+}
---
+++ Hangfire.Service/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hangfire.Common;
+using Hangfire.Service.Services;
+using Microsoft.Owin;
+using Owin;
+
+
+namespace Hangfire.Service
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHangfireDashboard();           
+            DelayedJobs();
+        }       
+
+        private void DelayedJobs()
+        {           
+            BackgroundJob.Schedule(() => new Feedback().Send("10 seconds"), TimeSpan.FromSeconds(10));
+            BackgroundJob.Schedule(() => new Feedback().Send("20 seconds"), TimeSpan.FromSeconds(20));
+            BackgroundJob.Schedule(() => new Feedback().Send("1 minute"), TimeSpan.FromMinutes(1));
+            BackgroundJob.Schedule(() => new Feedback().Send("2 minutes"), TimeSpan.FromMinutes(2));
+            BackgroundJob.Schedule(() => new Feedback().Send("5 minutes"), TimeSpan.FromMinutes(5));
+            BackgroundJob.Schedule(() => new Feedback().Send("1 hour"), TimeSpan.FromHours(1));
+        }
+    }
+}
